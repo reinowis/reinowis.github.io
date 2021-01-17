610897e66569e2be84cf0d6b11db8d2d26b25a76
reinomai <mousewis@gmail.com>
2021-01-17 11:39:47 +0700

    edit: fonts, menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,7 +114,7 @@
 	$("a").on('click', function (event) {
 
 		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
+		if (this.hash !== "" && $(this.hash).length > 0) {
 			// Prevent default anchor click behavior
 			event.preventDefault();
 
